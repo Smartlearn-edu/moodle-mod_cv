02c0e315d73159c322b05d47201e226e11d93b91
Smartlearn-edu <onlymade@gmail.com>
2026-09-10 05:08:33 +0300

    v2

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_cv';
-$plugin->version = 2026091000;
+$plugin->version = 2026091001;
 $plugin->requires = 2022112800; // Requires Moodle 4.1 or higher.
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v0.3.0';
