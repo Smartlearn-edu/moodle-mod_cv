5fdcab4ba66457574c6428ab486e1d3b57160679
Smartlearn-edu <onlymade@gmail.com>
2026-09-26 09:41:43 +0300

    no pdf

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_cv';
-$plugin->version = 2026092601;
+$plugin->version = 2026092602;
 $plugin->requires = 2022112800; // Requires Moodle 4.1 or higher.
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v0.5.9';
